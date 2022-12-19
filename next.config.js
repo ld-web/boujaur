e7f1401b0f13ec0297f593a458000ec5f4503722
Lucas Delobelle <lucas@ld-web.net>
2022-12-19 07:10:50 +0100

    :globe_with_meridians: prepared locales in config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  i18n: {
+    locales: ["fr", "zh"],
+    defaultLocale: "fr",
+    localeDetection: false,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
