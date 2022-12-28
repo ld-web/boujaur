02d03637b5fb0f2e360e46b3974bff622b57943e
Lucas Delobelle <lucas@ld-web.net>
2022-12-28 12:25:51 +0100

    :art: improve Layout code appearance

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import Head from "next/head";
 
-const Layout = ({ children }: { children?: React.ReactNode }) => (
-  <>
-    <Head>
-      <title>BOUJAUR</title>
-      <meta name="description" content="Boujaur - 關於台灣" />
-      <link rel="icon" href="/taiwan_flag.png" />
-    </Head>
+const Layout = ({ children }: { children?: React.ReactNode }) => {
+  return (
+    <>
+      <Head>
+        <title>BOUJAUR</title>
+        <meta name="description" content="Boujaur - 關於台灣" />
+        <link rel="icon" href="/taiwan_flag.png" />
+      </Head>
 
-    <main className="grid place-content-center mx-2.5 sm:mx-0">{children}</main>
-  </>
-);
+      <main className="grid place-content-center mx-2.5 sm:mx-0">
+        {children}
+      </main>
+    </>
+  );
+};
 
 export default Layout;
