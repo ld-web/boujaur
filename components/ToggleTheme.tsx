261c962c1c5518836b61d0143a9c74a5853ff491
Lucas Delobelle <lucas@ld-web.net>
2022-12-29 09:04:44 +0100

    :bug: fix system theme + toggle theme updates
    State var, useEffect to better have better theme synchronization

--- components/ToggleTheme.tsx
+++ components/ToggleTheme.tsx
@@ -1,15 +1,25 @@
 import { useTheme } from "next-themes";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+
+const iconPath = (filename: string): string => `/images/${filename}.svg`;
 
 const ToggleTheme = () => {
   const { theme, setTheme } = useTheme();
+  const [themeIcon, setThemeIcon] = useState(iconPath("moon"));
 
   const toggleTheme = () => setTheme(theme === "dark" ? "light" : "dark");
+
+  useEffect(() => {
+    setThemeIcon(() =>
+      theme === "light" ? iconPath("moon") : iconPath("sun")
+    );
+  }, [theme]);
 
   return (
     <button type="button" onClick={toggleTheme}>
       <Image
-        src={theme === "light" ? "/images/moon.svg" : "/images/sun.svg"}
+        src={themeIcon}
         alt="Retour"
         width="27"
         height="27"
