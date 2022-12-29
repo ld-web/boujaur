1212ef7abbd61416ef8cef6030a811e5323dfbc7
Lucas Delobelle <lucas@ld-web.net>
2022-12-29 10:01:10 +0100

    :memo: OG tags

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,6 +8,9 @@ const Layout = ({ children }: { children?: React.ReactNode }) => {
         <title>BOUJAUR</title>
         <meta name="description" content="Boujaur - 關於台灣" />
         <link rel="icon" href="/taiwan_flag.png" />
+        <meta property="og:title" content="Boujaur - 關於台灣" />
+        <meta property="og:description" content="台灣冒險" />
+        <meta property="og:image" content="/images/preview.jpg" />
       </Head>
 
       <main className="grid place-content-center mx-2.5 sm:mx-0">
