5c6d486c65ee64143786909c20943662cb6ed917
Lucas Delobelle <lucas@ld-web.net>
2023-09-19 22:07:46 +0200

    :heavy_plus_sign: add Vercel Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import Layout from "../components/Layout";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class" enableSystem={false}>
       <Layout>
         <Component {...pageProps} />
+        <Analytics />
       </Layout>
     </ThemeProvider>
   );
